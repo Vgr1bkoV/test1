9dbe1a7e2ed2138b4293c7862b3daca33a3fecd6
Владимир Грибков <you@example.com>
2023-02-22 18:48:39 +0400

    2lesson

--- DZ1/index.js
+++ DZ1/index.js
@@ -1,31 +1,140 @@
-const globalState = {};
-const LS = localStorage;
+window.application = {
+  renderBlock: function(name, container) {
+    window.application[name](container);
+  },
+}
 
-const difLevelNumber1 = document.querySelector('.difficult_level__number_1');
-const difLevelNumber2 = document.querySelector('.difficult_level__number_2');
-const difLevelNumber3 = document.querySelector('.difficult_level__number_3');
+const globalState = {
+  difficult:1,
+};
 
-const buttonStart = document.querySelector('.button_start');
+function renderMainScreen(container){
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Главный экран';
+  container.appendChild(h1);
+}
 
-buttonStart.addEventListener('click',(event) => {
-  event.preventDefault();
-  window.location.href = '/game.html';
-});
+function renderFirstPage(container){ 
+  const div = document.createElement('div');
+  div.classList.add('square');
 
-difLevelNumber1.addEventListener('click', (event) =>{
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Выбери сложность';
+  h1.classList.add('chose_difficult');
+
+  const divDif = document.createElement('div');
+  divDif.classList.add('difficult_level');
+
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  const div3 = document.createElement('div');
+  div1.textContent = '1';
+  div2.textContent = '2';
+  div3.textContent = '3';
+  div1.classList.add('difficult_level__number');
+  div1.addEventListener('click', () => {
     console.log("1");
     globalState.difficult = 1;
-    LS.setItem('global', JSON.stringify(globalState));
-  });
-
-  difLevelNumber2.addEventListener('click', (event) =>{
+  })
+  div2.classList.add('difficult_level__number');
+  div2.addEventListener('click', () => {
     console.log("2");
     globalState.difficult = 2;
-    LS.setItem('global', JSON.stringify(globalState));
-  });
-
-  difLevelNumber3.addEventListener('click', (event) =>{
+  })
+  div3.classList.add('difficult_level__number');
+  div3.addEventListener('click', () => {
     console.log("3");
     globalState.difficult = 3;
-    LS.setItem('global', JSON.stringify(globalState));
-  });
+  })
+
+  const button = document.createElement('button');
+  button.textContent = 'Старт';
+  button.classList.add('button');
+  button.addEventListener('click', () => {
+    console.log('click');
+    container.innerHTML = ''; 
+    renderMainScreen(container);
+  })
+
+  container.appendChild(div);
+
+  div.appendChild(h1);
+
+  div.appendChild(divDif);
+  divDif.appendChild(div1);
+  divDif.appendChild(div2);
+  divDif.appendChild(div3);
+
+  div.appendChild(button);
+} 
+
+window.application['first-page'] = renderFirstPage;
+
+window.application.renderBlock('first-page', document.querySelector('.app'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const difLevelNumber1 = document.querySelector('.difficult_level__number_1');
+// const difLevelNumber2 = document.querySelector('.difficult_level__number_2');
+// const difLevelNumber3 = document.querySelector('.difficult_level__number_3');
+
+// const buttonStart = document.querySelector('.button_start');
+
+// buttonStart.addEventListener('click',(event) => {
+//   event.preventDefault();
+//   window.location.href = '/game.html';
+// });
+
+// difLevelNumber1.addEventListener('click', (event) =>{
+//     console.log("1");
+//     globalState.difficult = 1;
+//   });
+
+//   difLevelNumber2.addEventListener('click', (event) =>{
+//     console.log("2");
+//     globalState.difficult = 2;
+//   });
+
+//   difLevelNumber3.addEventListener('click', (event) =>{
+//     console.log("3");
+//     globalState.difficult = 3;
+//   });
